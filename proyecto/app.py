8768417068d3d56b186fead7fdd9eb0673077562
PatrickC96 <patrick.cabezas@epn.edu.ec>
2020-08-10 17:43:46 -0500

    graficos

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, url_for, redirect, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from camera import VideoCamera
-import cv2
+
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
+
+video_camera = None
+global_frame = None
 
 @app.route('/')
 def home():
@@ -17,6 +23,24 @@ def administrador():
 @app.route('/usuario')
 def usuario():
     return render_template('usuario.html')
+    
+@app.route('/graficos')
+def graficos():
+    return render_template('graficos.html')
+
+
+@app.route('/build_plot')
+def build_plot():
+
+    img = io.BytesIO()
+
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+    plt.plot(x,y)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    return img.getvalue()
+
 
 @app.route('/delete/<string:id>', methods = ['POST','GET'])
 def delete_contact(id):
@@ -34,6 +58,30 @@ def get_contact(id):
     cur.close()
     print(data[0])
     return render_template('edit-contact.html', contact = data[0])
+
+def video_stream():
+    global video_camera 
+    global global_frame
+
+    if video_camera == None:
+        video_camera = VideoCamera()
+        
+    while True:
+        frame = video_camera.get_frame()
+
+        if frame != None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+
+@app.route('/video_viewer')
+def video_viewer():
+    return Response(video_stream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- proyecto/camera.py
+++ proyecto/camera.py
@@ -1,29 +1,29 @@
 import cv2
 import threading
 
-# class RecordingThread (threading.Thread):
-#     def __init__(self, name, camera):
-#         threading.Thread.__init__(self)
-#         self.name = name
-#         self.isRunning = True
+class RecordingThread (threading.Thread):
+    def __init__(self, name, camera):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.isRunning = True
 
-#         self.cap = camera
-#         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-#         self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
+        self.cap = camera
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        self.out = cv2.VideoWriter('./static/video.avi',fourcc, 20.0, (640,480))
 
-#     def run(self):
-#         while self.isRunning:
-#             ret, frame = self.cap.read()
-#             if ret:
-#                 self.out.write(frame)
+    def run(self):
+        while self.isRunning:
+            ret, frame = self.cap.read()
+            if ret:
+                self.out.write(frame)
 
-#         self.out.release()
+        self.out.release()
 
-#     def stop(self):
-#         self.isRunning = False
+    def stop(self):
+        self.isRunning = False
 
-#     def __del__(self):
-#         self.out.release()
+    def __del__(self):
+        self.out.release()
 
 class VideoCamera(object):
     def __init__(self):
@@ -31,11 +31,11 @@ class VideoCamera(object):
         self.cap = cv2.VideoCapture(0)
       
         # Initialize video recording environment
-        # self.is_record = False
-        # self.out = None
+        self.is_record = False
+        self.out = None
 
         # Thread for recording
-        # self.recordingThread = None
+        self.recordingThread = None
     
     def __del__(self):
         self.cap.release()
@@ -65,6 +65,9 @@ class VideoCamera(object):
         else:
             return None
 
+    def __del__(self):
+        # releasing camera
+        self.cap.release()
     # def start_record(self):
     #     self.is_record = True
     #     self.recordingThread = RecordingThread("Video Recording Thread", self.cap)
@@ -72,7 +75,6 @@ class VideoCamera(object):
 
     # def stop_record(self):
     #     self.is_record = False
-
     #     if self.recordingThread != None:
     #         self.recordingThread.stop()
 
