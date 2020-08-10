b7f7bdd4ddd2481eeba72833a1c18decdbbcfba2
PatrickC96 <patrick.cabezas@epn.edu.ec>
2020-08-10 13:41:03 -0500

    camara

--- proyecto/app.py
+++ proyecto/app.py
@@ -5,8 +5,6 @@ import cv2
 
 app = Flask(__name__)
 
-video_stream = VideoCamera()
-
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -19,16 +17,6 @@ def administrador():
 @app.route('/usuario')
 def usuario():
     return render_template('usuario.html')
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(video_stream),
-                mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/delete/<string:id>', methods = ['POST','GET'])
 def delete_contact(id):
